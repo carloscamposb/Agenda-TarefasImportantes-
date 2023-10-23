f094c8d31f44cf0fa7b5c199c17546ecba24bb8b
Carlos Campos <108171029+carloscamposb@users.noreply.github.com>
2023-10-22 23:02:07 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Olá pessoal! 👦🏻
+------------------
+Esse programa é uma agenda para estudos ✍🏻 
+trabalha um sistema básico de *CRUD* e serve para auxiliar a guardar aquelas atividades importantes!! 
+
+Utiliza *Html | CSS | JavaScript*
+
+
+
---
+++ script.js
@@ -0,0 +1,62 @@
+// Obtém referências para elementos HTML
+const form = document.getElementById("FormAgenda");
+const listaDeAtividades = document.getElementById("listaDeAtividades");
+
+// Armazenamento de dados 
+const activities = [];
+
+// Função para adicionar uma nova atividade
+function addActivity(data) {
+    activities.push(data);
+    displayActivities();
+}
+
+// Função para exibir atividades na lista
+function displayActivities() {
+    listaDeAtividades.innerHTML = "";
+    activities.forEach((activity, index) => {
+        listaDeAtividades.innerHTML += `
+            <li>${activity.disciplina} : ${activity.atividade} - ${activity.data}
+                <button onclick="editActivity(${index})">Editar</button>
+                <button onclick="deleteActivity(${index})">Excluir</button>
+            </li>
+        `;
+    });
+}
+
+// Função para editar uma atividade
+function editActivity(index) {
+    const activity = activities[index];
+    document.getElementById("data").value = activity.data;
+    document.getElementById("disciplina").value = activity.disciplina;
+    document.getElementById("atividade").value = activity.atividade;
+
+
+    // Atualiza a atividade quando o botão Atualizar é clicado
+    document.querySelector("button[type=button]").style.display = "none";
+    document.getElementById("btnAtualizar").style.display = "inline";
+    document.getElementById("btnAtualizar").onclick = () => {
+        activity.data = document.getElementById("data").value;
+        activity.disciplina = document.getElementById("disciplina").value;
+        activity.atividade = document.getElementById("atividade").value;
+        form.reset();
+        document.querySelector("button[type=button]").style.display = "inline";
+        document.getElementById("btnAtualizar").style.display = "none";
+        displayActivities();
+    };
+}
+
+// Função para excluir uma atividade
+function deleteActivity(index) {
+    activities.splice(index, 1);
+    displayActivities();
+}
+
+// adiciona uma nova atividade à agenda
+document.querySelector("button[type=button]").addEventListener("click", () => {
+    const data = document.getElementById("data").value;
+    const disciplina = document.getElementById("disciplina").value;
+    const atividade = document.getElementById("atividade").value;
+    addActivity({ data, disciplina, atividade });
+    form.reset();
+});
